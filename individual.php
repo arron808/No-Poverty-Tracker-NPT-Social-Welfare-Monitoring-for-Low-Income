8038cb0b89df4e20d2f15d746ad46d3c043177bb
Ehrin- John <ehrinjohn08@gmail.com>
2025-05-04 00:33:38 +0800

    Reapply "file fix"
    This reverts commit 846c4b269d04da000d498f1e37f39e41d7906806.

---
+++ individual.php
@@ -0,0 +1,33 @@
+<?php
+
+class Individual {
+    private $conn;
+
+    public function __construct($db) {
+        $this->conn = $db;
+    }
+
+    public function create($household_id, $name, $dob, $gender, $education_level, $employment_status, $disability) {
+        try {
+            $stmt = $this->conn->prepare("CALL CreateIndividual(:household_id, :name, :dob, :gender, :education, :employment, :disability)");
+            $stmt->execute([
+                ':household_id' => $household_id,
+                ':name' => $name,
+                ':dob' => $dob,
+                ':gender' => $gender,
+                ':education' => $education_level,
+                ':employment' => $employment_status,
+                ':disability' => $disability
+            ]);
+            return true;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+
+    public function getAll() {
+        $stmt = $this->conn->query("CALL GetAllIndividuals()");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+?>
